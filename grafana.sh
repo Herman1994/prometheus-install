738e2e8f75eae233d38b64066af34eaa3bde62d2
Jiří Jehlář <33741898+Herman1994@users.noreply.github.com>
2023-07-19 00:13:43 +0200

    Update grafana.sh

--- grafana.sh
+++ grafana.sh
@@ -2,10 +2,10 @@
 
 # Download grafana
 sudo apt-get install -y adduser libfontconfig1
-wget https://dl.grafana.com/oss/release/grafana_8.1.1_amd64.deb
+wget https://dl.grafana.com/oss/release/grafana_10.0.2_amd64.deb
 
 # Install grafana
-sudo dpkg -i grafana_8.1.1_amd64.deb
+sudo dpkg -i grafana_10.0.2_amd64.deb
 
 # systemd
 sudo systemctl daemon-reload
@@ -13,4 +13,4 @@ sudo systemctl enable grafana-server
 sudo systemctl start grafana-server
 
 # Installation cleanup
-rm grafana_8.1.1_amd64.deb
+rm grafana_10.0.2_amd64.deb
